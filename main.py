ed2d3442a0eeaa5ab1776a32b2c706d734b32cf6
Alexandre <alexandresilvestrin16@gmail.com>
2022-10-20 21:52:06 -0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import formcheck ,btn
-
+from Email import criarRelatorio
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dc9b08d33bd9c8dbf72a933a368352c8'
 
@@ -17,17 +17,10 @@ def home():
 @app.route('/forms', methods=['GET', 'POST'])
 def form():
     forms = formcheck()
-
     if forms.validate_on_submit() and 'btnFinalizar' in request.form:
         # verifica se o botao foi precisonado com td preenchido(validators do forms erificar se esta preenchido)
         flash('Formulario Salvo')
-        print(f'nome : {forms.nome.data}')
-        print(f'email : {forms.email.data}')
-        print(f'perg1 : {forms.perg1.data}')
-        print(f'perg2 : {forms.perg2.data}')
-        print(f'perg3 : {forms.perg3.data}')
-        print(f'perg4 : {forms.perg4.data}')
-        print(f'perg5 : {forms.perg5.data}')
+        criarRelatorio()
     return render_template('forms.html', forms=forms)
 
 
